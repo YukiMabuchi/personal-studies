34eba6be13b2f559aa15c43c8ce248615a1d9a57
YukiMabuchi <yuk1mabuchi1999@gmail.com>
2024-06-26 08:31:46 +0900

    python basic: unittest

---
+++ python/basics/test/unittest_.py
@@ -0,0 +1,6 @@
+class Cal(object):
+    def add_num_and_double(self, x, y):
+        if type(x) is not int or type(y) is not int:
+            raise ValueError
+        
+        return (x + y) * 2
---
+++ python/basics/test/unittest_test.py
@@ -0,0 +1,38 @@
+"""
+DOC: https://docs.python.org/3/library/unittest.html
+
+unittest.TestCaseを継承しテストができる
+setUpとtearDownで各テストfunctionが実行される前と後にそれぞれ処理を行える
+@unittest.skipデコレーターでテストをスキップできる
+"""
+
+import unittest
+
+import unittest_
+
+version = 1
+
+class CalTest(unittest.TestCase):
+    def setUp(self):
+        print('set up')
+        self.cal = unittest_.Cal()
+
+    def tearDown(self):
+        print('clean up')
+        del self.cal
+    
+    # @unittest.skip('skip!')
+    @unittest.skipIf(version==1, 'skip for version 1')
+    def test_add_num_and_double(self):
+        self.assertEqual(
+            self.cal.add_num_and_double(1, 1),
+            4
+        )
+    
+    # 例外チェック
+    def test_add_num_and_double_raise(self):
+        with self.assertRaises(ValueError):
+            self.cal.add_num_and_double('1', '1')
+
+if __name__ == '__main__':
+    unittest.main()
