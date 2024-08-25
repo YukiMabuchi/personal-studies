86367aafc5842a1488da4d95a28b2ecd8e814b61
Yuki Mabuchi <67894806+YukiMabuchi@users.noreply.github.com>
2024-08-25 17:22:09 +0900

    add user_data

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,16 @@ resource "aws_instance" "example" {
   ami           = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
 
+  # heredoc文法
+  user_data = <<-EOF
+            #!/bin/bash
+            echo "Hello World" > index.html
+            nohup busybox httpd -f -p 8080 &
+            EOF
+
+  # user_dataに新しいパラメータを渡してapplyした時下のインスタンスをターミネートして新しいものを起動する（ユーザーデータは最初の起動時しか実行されない）
+  user_data_replace_on_change = true
+
   tags = {
     Name = "terraform-example"
   }
